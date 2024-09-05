14843e51adff0423b1c9d6b6b9d721b05f049b6e
comedicomedy <bsinex389@gmail.com>
2024-09-05 18:58:54 -0400

    Added Docker And Cache
    Added support for a docker image and added cache so results are someone
    accurate.

--- index.ts
+++ index.ts
@@ -1,40 +1,38 @@
 import fs from "fs/promises";
 
-var  numOfVisits = await fs.readFile("visits").then((res) => {
+var numOfVisits = await fs.readFile("visits").then((res) => {
     return parseInt(res.toString());
 }).catch(() => {
     return 0;
 });
 
-const server =  Bun.serve({
+const server = Bun.serve({
     port: 8080,
-    
+
     fetch(req) {
-                
-        if (new URL(req.url).pathname  === "/visits"){
+
+        if (new URL(req.url).pathname === "/visits") {
             numOfVisits++;
 
-            let visits = {"numOfVisits": numOfVisits} 
+            let visits = { "numOfVisits": numOfVisits }
             let jsonResponse = Response.json(visits);
 
-            if (numOfVisits % 10 == 0){
-                fs.writeFile("visits", numOfVisits.toString()).catch(() => {console.error("Didn't Write Data")});
+            if (numOfVisits % 10 == 0) {
+                fs.writeFile("visits", numOfVisits.toString()).catch(() => { console.error("Didn't Write Data") });
                 console.log("Wrote Data To File")
             }
             console.log(visits.numOfVisits);
 
-            jsonResponse.headers.set("Access-Control-Allow-Origin", "*")
-            return jsonResponse; 
+            jsonResponse.headers.set("Access-Control-Allow-Origin", "*");
+            jsonResponse.headers.set("Cache-Control", "max-age=600");
+            return jsonResponse;
         }
-        
-    
-      return Response.error();
+
+
+        return Response.error();
     },
 
 })
-
-
-
 
 console.log(server.port);
 console.log(server.url);
